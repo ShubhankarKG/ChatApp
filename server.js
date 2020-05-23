2a1147ec2d76e080f6624d4f50d7bb5e5d077f48
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-05-23 15:57:57 +0530

    Changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
